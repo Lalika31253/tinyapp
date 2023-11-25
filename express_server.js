24386fa2f5416fb7b5ba7ac2f2dd45342ceb30cb
Lalika31253 <Lalika31253@gmail.com>
2023-11-25 05:26:36 -0700

    Fill out the urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { Template } = require("ejs");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -7,8 +8,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; //keep track of a URLs
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
