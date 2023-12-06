5130e476f935814e71c2bb3b2a618f34d12e37de
Lalika31253 <Lalika31253@gmail.com>
2023-12-05 22:15:36 -0700

    Add functionality for user to see only users created links

--- express_server.js
+++ express_server.js
@@ -57,10 +57,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const user_id = req.session.user_id; //set user id cookie
 
+  if (!user_id) { //check if user is not logged in
+    return res.redirect('/login');
+  };
+
+  //get URLs specific to the logged in user 
+  const usersUrls = urlsForUser (urlDatabase, user_id);
   const templateVars = {
-    urls: urlDatabase, //keep track of a URLs
-    user: users[user_id],
-    user_id: req.session.user_id
+    urls: usersUrls, //pass URLs
+    user: users[user_id] //pass user from the object
   };
 
   res.render("urls_index", templateVars);
@@ -76,7 +81,7 @@ app.get("/urls/new", (req, res) => {
   };
 
   const templateVars = {
-    user: users[user_id], //pass user from the object
+    user: users[user_id], //pass user information from the object
   };
 
   res.render("urls_new", templateVars);
@@ -85,15 +90,12 @@ app.get("/urls/new", (req, res) => {
 
 //POST route to receive the form submission of new URL
 app.post("/urls", (req, res) => {
+  //generate new short id for the URL using helper function
   const newShortId = generateRandomString(allCharacters, strLength);
   const newLongURL = req.body.longURL;
   const user_id = req.session.user_id;
 
-  if (!user_id) {
-    return res.redirect('/login');
-  };
-
-  // //save data to our data base
+  // save new URL to our data base
   urlDatabase[newShortId] = { longURL: newLongURL, userID: user_id };
   res.redirect('/urls');
 });
@@ -106,18 +108,20 @@ app.get("/u/:id", (req, res) => {
 });
 
 
-//rout to display a single URL and its details
+//route to display a single URL and its details
 app.get("/urls/:id", (req, res) => {
 
   const user_id = req.session.user_id;
 
+  //check if the user is not the owner of the URL
   if (user_id !== (urlDatabase[req.params.id] && urlDatabase[req.params.id].userID)) {
     return res.redirect('/login');
   };
+
   const longURL = urlDatabase[req.params.id].longURL;
-  if (!longURL) {
+  if (!longURL) { //check if no long URL is found
     return res.status(404).send("No link with this ID found!");
-  }
+  };
   const templateVars = {
     id: req.params.id,
     longURL,
@@ -128,37 +132,24 @@ app.get("/urls/:id", (req, res) => {
 });
 
 
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-
-//Home page
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 //POST route for /urls/:id to update a resource
 app.post("/urls/:id", (req, res) => {
   const newLongURL = req.body.newURL;
   const urlID = req.params.id;
   const user_id = req.session.user_id;
 
-  if (!user_id) { //check if user is logged in
+  if (!user_id) { //check if user is not logged in
     return res.status(400).send("Please log in");
   };
 
-  if (!urlDatabase[urlID]) { //check if URL ID exists in DB
-    return res.status(404).send("URL ID not found");
+  if (!urlDatabase[urlID]) { 
+    return res.status(403).send("No link with this ID found!");
   };
 
   if (urlDatabase[urlID].userID !== user_id) { // check if user owns the URL
     return res.status(403).send("You don't own this URL");
   };
 
-  //update
   urlDatabase[urlID] = { longURL: newLongURL, userID: user_id };
 
   res.redirect("/urls");
@@ -170,7 +161,7 @@ app.post("/urls/:id/delete", (req, res) => {
   const urlID = req.params.id;
   const user_id = req.session.user_id;
 
-  if (!user_id) { //check if user is logged in
+  if (!user_id) { //check if user is not logged in
     return res.status(400).send("Please log in");
   };
 
@@ -229,6 +220,18 @@ app.post("/login", (req, res) => {
 
 });
 
+app.get('/urls', (req, res) => {
+  const user_id = req.session.user_id;
+  if (!user_id) {
+    return res.status(400).send('User is not logged in!');
+  };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[user_id],
+  };
+  res.render('urls_index', templateVars);
+});
+
 
 //POST to handle user logout
 app.post("/logout", (req, res) => {
@@ -253,27 +256,28 @@ app.get("/register", (req, res) => {
 });
 
 
-//POST to handle a registration page
+//POST route to handle a registration page
 app.post("/register", (req, res) => {
-  //pull data off the body object
+  //generate random user id using helper function
   const userId = generateRandomString(allCharacters, strLength);
   const email = req.body.email;
   const password = req.body.password;
+
+  //generate a salt and hash the password using bcrypt for security
   const salt = bcrypt.genSaltSync(10);
   const hashed = bcrypt.hashSync(password, salt);
 
-  if (!email || !password) {
+  if (!email || !password) { 
     return res.status(400).send("Provide email and a password");
   };
 
   //lookup the user based on provided email
   const user = getUserByEmail(email, users);
-
   if (user) {
     return res.status(400).send("The user with such an email already exists");
   };
 
-  //Passing user into to users object
+  //Passing a new user into to users object
   users[userId] = { id: userId, email: email, password: hashed }; //add use to database
 
   req.session.user_id = userId;  //setting the user id cookies
